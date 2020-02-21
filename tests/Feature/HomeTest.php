a8bdf27a23ac648585f6172d4d95312f9f6cc927
Emile LEPETIT <emile.lepetit@gmail.com>
2020-02-21 22:25:16 +0100

    Add Browser Tests && Ci

--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -2,20 +2,23 @@
 
 namespace Tests\Feature;
 
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class HomeTest extends TestCase
 {
+    // use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testHomeRedirectTest()
     {
         $response = $this->get('/');
-
-        $response->assertStatus(200);
+        $response->assertStatus(302);
     }
 }
